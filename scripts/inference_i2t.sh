8e79667872d38aa04072f382e9d1261f1b987a36
王劲 <wangjin@wangjindeMacBook-Pro.local>
2025-07-15 20:15:06 +0800

    init

--- scripts/inference_i2t.sh
+++ scripts/inference_i2t.sh
@@ -1,8 +1,10 @@
+CKPT_PATH=/mnt/a100/datasets1/wangjin/pretrained_model/FUDOKI
+
 torchrun --nproc_per_node 1 inference_i2t_local.py \
     --batch_size 1 \
-    --checkpoint_path /cache/fudoki \
-    --text_embedding_path /cache/text_embedding.pt \
-    --image_embedding_path  /cache/image_embedding.pt \
+    --checkpoint_path $CKPT_PATH \
+    --text_embedding_path $CKPT_PATH/text_embedding.pt \
+    --image_embedding_path  $CKPT_PATH/image_embedding.pt \
     --image_path asset/0.jpg \
-    --discrete_fm_steps 32 \
-    --output_dir /cache/fudoki_output
+    --discrete_fm_steps 50 \
+    --output_dir ./fudoki_output
